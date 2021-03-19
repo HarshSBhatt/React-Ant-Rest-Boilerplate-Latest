ff1214301da596aac2d319d9908ec25fd36fdd05
harshb1 <harshb@logicwind.com>
2021-03-19 12:44:40 +0530

    made app PWA

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import client from "./apollo";
 import Routes from "./Routes";
 import { AppContextProvider } from "./AppContext";
 import reportWebVitals from "./reportWebVitals";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 //! Styles
 
@@ -30,6 +31,11 @@ ReactDOM.render(
   </ApolloProvider>,
   document.getElementById("root")
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
